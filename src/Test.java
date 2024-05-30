e8ccc5d9fd13fee1f4553bd0f9953507ababa4ad
HEO <cloudisme99@gmail.com>
2024-05-30 10:28:50 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+	/** 자바독
+	 * @author 굿
+	 * @param args
+	 * @deprecated 오
+	 * @apiNote 기능
+	 **/
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println(a);
+	}
+}
+
+
+
+
