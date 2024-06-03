1d11dbbd5e43b884be0edb638b16b53155d26158
HEO <cloudisme99@gmail.com>
2024-06-03 23:44:25 +0900

    240530

---
+++ src/practice_240530.java
@@ -0,0 +1,48 @@
+public class practice_240530 implements TestInterface {
+
+  public static void main(String[] args) {
+    // char c1 = 'A';            // 문자를 직접 저장
+    // char c2 = 65;            // 10진수로 저장
+    // char c3 = '\u0041';        // 16진수로 저장
+    //
+    // char c4 = '가';            // 문자를 직접 저장
+    // char c5 = 44032;        // 10진수로 저장
+    // char c6 = '\uac00';        // 16진수로 저장
+    //
+    // int unicode = c1;        // 유니코드 얻기
+    //
+    // System.out.println(c1);
+    // System.out.println(c2);
+    // System.out.println(c3);
+    // System.out.println(c4);
+    // System.out.println(c5);
+    // System.out.println(c6);
+    // System.out.println(unicode);
+
+    // short sVal = 10;
+    // short result = (short) (sVal + 10);
+    // System.out.println(result);
+
+    // float myFloat = 9.75f;
+    //
+    // float anotherFloat = myFloat * 2;
+    //
+    // System.out.println("Original float value: " + (int)myFloat);
+    // System.out.println("Calculated float value: " + (byte)anotherFloat);
+
+    boolean isTrue = true;
+    if (isTrue) {
+      System.out.println("이건 ture 입니다.");
+    } else {
+      System.out.println("이건 false 입니다.");
+    }
+
+    int a = 10;
+    }
+
+  @Override
+  public void test() {
+
+  }
+}
+
