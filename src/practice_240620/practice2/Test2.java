d03db70cd25a7206b52262483d2449b31d125cbf
HEO <cloudisme99@gmail.com>
2024-06-20 23:06:27 +0900

    230620_Thread실습1

---
+++ src/practice_240620/practice2/Test2.java
@@ -0,0 +1,19 @@
+package practice_240620.practice2;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		MyThread myThread = new MyThread("스레드");
+		MyThread myThread1 = new MyThread("스레드1");
+		MyThread myThread2 = new MyThread("스레드2");
+		MyThread myThread3 = new MyThread("스레드3");
+		MyThread myThread4 = new MyThread("스레드4");
+
+		myThread.start();
+		myThread1.start();
+		myThread2.start();
+		myThread3.start();
+		myThread4.start();
+
+	}
+}
